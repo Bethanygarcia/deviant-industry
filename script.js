5ea76c46a2d3fe8d8bfafc4132e09738b858604f
Bethany Garcia <bethanygarcia817@gmail.com>
2019-05-06 21:27:57 +0000

    first draft

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // JavaScript File
 /*global $*/
+/*global localStorage*/
 
 $("#submit").click(function() {
+    var input_genre = $("fav_genre").val();
+    var input_era = $("fav_era").val();
+    var input_artist = $("fav_artist").val();
     
+    var starting_quiz_answers = {
+        favorite_genre: input_genre,
+        favorite_era: input_era,
+        favorite_artist: input_artist,
+    };
+    
+    var user_object_string= JSON.stringify(starting_quiz_answers);
+    
+    localStorage.setItem("stored_answers", user_object_string);
 });
+
+function access_stored_answers() {
+    var stored_object_string= localStorage.getItem("stored_answers");
+    var object_user_answers= JSON.parse(stored_object_string);
+};
+
+access_stored_answers();
